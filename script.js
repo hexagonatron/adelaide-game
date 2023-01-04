586e67ca15a9592545a0c967b3e086f90bd868c9
Ben <46476247+hexagonatron@users.noreply.github.com>
2023-01-04 23:09:05 +1030

    map coloring.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const getChecked = () => {
+    return document.querySelector("input[name=player]:checked").value;
+}
 const map = L.map('adelaide-map', {
     attributionControl: false,
 }).setView([-34.9285, 138.6007], 12);
@@ -8,9 +11,18 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     maxZoom: 20
 }).addTo(map);
 
-L.geoJSON(suburbs).addTo(map);
-
-suburbs.features.forEach(suburb => {
-    // L.tooltip(suburb.properties.center, {content: suburb.properties.suburb}).addTo(map);
-})
-// L.tooltip({permanent: true}).setLatLng({lon: 138.53080529039528,lat: -34.94615527120816}).setContent("test").addTo(map);
+L.geoJSON(suburbs, {
+    style: (feature) => {
+        return {
+            color: 'black',
+            weight: 1,
+            fillOpacity: 0,
+        }
+    },
+    onEachFeature: (feature, layer) => {
+        layer.addEventListener('click', (e) => {
+            const checked = getChecked();
+            layer.setStyle({color: checked, fillOpacity: 0.2, weight: 2});
+        })
+    }
+}).addTo(map);
